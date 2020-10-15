95205bce887f8d317f93de4fed0942e8fedc5daf
Sannim <inegg.apps@gmail.com>
2020-10-15 21:02:09 +0900

    화살표 함수

---
+++ 01-01-js-modern/07-arrow-function.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => a - b; // 한 줄로 축약이 가능하다
+console.log(sub(2, 2));
+
+const sum = add(1, 2);
+console.log(sum); // 3
+
+const hello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+hello('velopert');
