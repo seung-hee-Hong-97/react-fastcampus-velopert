a3f3b6ab91da92a98c70442e1f7e01e7ba5c7c5e
Sannim <inegg.apps@gmail.com>
2020-10-27 21:08:05 +0900

    useState를 이용하여 Counter 컴포넌트 만들기

--- 01-05-react-beginner/src/App.js
+++ 01-05-react-beginner/src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
-    return (
-        <div className='App'>
-            <Wrapper>
-                {/* 주석 */}
-                <Hello
-                    // 주석
-                    name='react'
-                    color='red'
-                    isSpecial={true}
-                />
-                <Hello
-                    isSpecial
-                    // 값을 지정하지 않으면 자동으로 true값으로 인식
-                />
-                <Hello />
-            </Wrapper>
-        </div>
-    );
+    return <Counter />;
 }
 
 export default App;
