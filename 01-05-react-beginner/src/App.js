1971c5c70e72825090fa2b4c800e76af5533179d
Sannim <inegg.apps@gmail.com>
2020-11-01 14:24:00 +0900

    배열에 항목 추가하기 복습

--- 01-05-react-beginner/src/App.js
+++ 01-05-react-beginner/src/App.js
@@ -1,35 +1,67 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
-    const users = [
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+    const { username, email } = inputs;
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'velopert',
             email: 'public.velopert@gmail.com',
+            active: true,
         },
         {
             id: 2,
             username: 'sannim',
             email: 'inegg.apps@gmail.com',
+            active: false,
         },
         {
             id: 3,
             username: 'liz',
             email: 'liz@gmail.com',
+            active: false,
         },
-    ];
+    ]);
     const nextId = useRef(4); // 새로운 항목을 추가할 때는 id가 4인 것부터 시작
+
+    const onChange = ({ target }) => {
+        const { name, value } = target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
 
     const onCreate = () => {
         /*
           🤷‍♂️ useRef: useState로 굳이 관리할 필요가  없는 요소
           즉, 해당 값이 바뀌더라도 렌더링이 될 필요가 없는 요소는 useState보다는 useRef로 관리하는 것이 좋다
         */
-        console.log(nextId.current); // 4
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers([...users, user]);
+        setInputs({
+            username: '',
+            email: '',
+        });
         nextId.current += 1;
     };
-    return <UserList users={users} />;
+
+    return (
+        <>
+            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+            <UserList users={users} />
+        </>
+    );
 }
 
 export default App;
