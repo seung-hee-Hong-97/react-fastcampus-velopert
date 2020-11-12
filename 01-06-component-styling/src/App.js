3045e7333f5680a81e319aba07e393f4a068498f
Sannim <inegg.apps@gmail.com>
2020-11-12 21:59:40 +0900

    SCSS를 사용하여 컴포넌트 스타일링하기

---
+++ 01-06-component-styling/src/App.js
@@ -0,0 +1,14 @@
+import Button from './components/Button';
+import './App.scss';
+
+function App() {
+    return (
+        <div className='App'>
+            <div className='buttons'>
+                <Button>Button</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
