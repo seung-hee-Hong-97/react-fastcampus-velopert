5785947ddfb9b74c9bc76b67834e987f3fba1f21
Sannim <inegg.apps@gmail.com>
2020-11-14 21:03:57 +0900

    classNames 패키지 설치하여 클래스명 동적으로 다양하게 부여하는 코드.

--- 01-06-component-styling/src/App.js
+++ 01-06-component-styling/src/App.js
@@ -5,7 +5,9 @@ function App() {
     return (
         <div className='App'>
             <div className='buttons'>
+                <Button size='large'>Button</Button>
                 <Button>Button</Button>
+                <Button size='small'>Button</Button>
             </div>
         </div>
     );
