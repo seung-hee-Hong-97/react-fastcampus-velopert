7e10f01654640f012c3e8ebdc9e36e6320a70b1e
Sannim <inegg.apps@gmail.com>
2020-11-14 21:29:42 +0900

    className이 겹치지 않게 작성하는 방법 (가이드)

--- 01-06-component-styling/src/components/Button.js
+++ 01-06-component-styling/src/components/Button.js
@@ -4,18 +4,38 @@ import './Button.scss';
 
 //large, medium, small
 //color: blue, pink, gray
-function Button({ children, size, color, outline, fullWidth }) {
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
+    console.log(rest);
     return (
         <button
             className={classNames('Button', size, color, {
                 outline,
                 fullWidth,
             })}
+            {...rest} // 나머지 속성을 적용해 주겠다.
         >
             {children}
         </button>
     );
 }
+
+/*
+ 🤷‍♂️ className이 겹치지 않는 방법 (팁)
+
+ 1. 컴포넌트의 이름을 고유하게 지정한다.
+ 2. 최상위 엘리먼트의 클래스 이름을 컴포넌트 이름과 동일하게 지정한다.
+ 3. 그 내부에서 셀렉터를 사용한다.
+ ex)
+
+ .UserProfile{
+     .user{
+         img{}
+        .username{}
+     }
+     .about{}
+ }
+
+*/
 
 Button.defaultProps = {
     size: 'medium',
