eb66094fdf8bade3b7e8f7c0940daaad7af08908
Sannim <inegg.apps@gmail.com>
2020-11-15 15:17:21 +0900

    Styled components를 이용하여 버튼 컴포넌트 만들기

--- 01-06-component-styling/src/App.js
+++ 01-06-component-styling/src/App.js
@@ -1,104 +1,21 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import StyleButton from './components/StyleButton';
 
-const Circle = styled.div`
-    width: 5rem;
-    height: 5rem;
-    background: ${(props) => props.color || 'black'};
-    border-radius: 50%;
-    ${(props) =>
-        props.huge &&
-        css`
-            width: 10rem;
-            height: 10rem;
-        ` /* 
-            🤷‍♂️
-            위 Template Literal에서 또 Props를 필요로 할 수가 있다. 
-            그런데 Template Literal만 찍어버리면 Props를 참조할 수 없으므로
-            styled-components에서 제공하는 css를 이용한다.
-            */}
+const AppBlock = styled.div`
+    width: 512px;
+    margin: 0 auto;
+    margin-top: 4rem;
+    border: 1px solid black;
+    padding: 1rem;
 `;
 
 function App() {
     return (
-        <>
-            <Circle />
-            <Circle color='blue' huge />
-        </>
+        <AppBlock>
+            <StyleButton>버튼</StyleButton>
+        </AppBlock>
     );
 }
 
 export default App;
-
-// function App() {
-//     const [check, setCheck] = useState(false);
-//     const onChange = (e) => {
-//         setCheck(e.target.checked);
-//     };
-//     return (
-//         <div>
-//             <CheckBox onChange={onChange} checked={check}>
-//                 다음 약관에 모두 동의
-//             </CheckBox>
-//         </div>
-//     );
-// }
-
-// function App() {
-//     return (
-//         <div className='App'>
-//             <div className='buttons'>
-//                 <Button size='large'>Button</Button>
-//                 <Button>Button</Button>
-//                 <Button size='small'>Button</Button>
-//             </div>
-//             <div className='buttons'>
-//                 <Button color='gray' size='large'>
-//                     Button
-//                 </Button>
-//                 <Button color='gray'>Button</Button>
-//                 <Button color='gray' size='small'>
-//                     Button
-//                 </Button>
-//             </div>
-//             <div className='buttons'>
-//                 <Button color='pink' size='large'>
-//                     Button
-//                 </Button>
-//                 <Button color='pink'>Button</Button>
-//                 <Button color='pink' size='small'>
-//                     Button
-//                 </Button>
-//             </div>
-//             <div className='buttons'>
-//                 <Button size='large' outline>
-//                     Button
-//                 </Button>
-//                 <Button color='gray' outline>
-//                     Button
-//                 </Button>
-//                 <Button color='pink' size='small' outline={true}>
-//                     Button
-//                 </Button>
-//             </div>
-//             <div className='buttons'>
-//                 <Button size='large' fullWidth>
-//                     Button
-//                 </Button>
-//                 <Button size='large' color='gray' fullWidth>
-//                     Button
-//                 </Button>
-//                 <Button
-//                     size='large'
-//                     color='pink'
-//                     fullWidth={true}
-//                     onClick={() => {
-//                         console.log('클릭!');
-//                     }}
-//                 >
-//                     Button
-//                 </Button>
-//             </div>
-//         </div>
-//     );
-// }
