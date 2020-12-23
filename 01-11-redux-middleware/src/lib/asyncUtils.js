d92a05f3704d78e47ec14d8a3884428cee8cce94
Sannim <inegg.apps@gmail.com>
2020-12-23 22:16:50 +0900

    사용자 경험개선 - 유틸함수 작성하여 반복되는 코드 줄이

--- 01-11-redux-middleware/src/lib/asyncUtils.js
+++ 01-11-redux-middleware/src/lib/asyncUtils.js
@@ -27,6 +27,43 @@ export const createPromiseThunk = (
     return thunkCreator;
 };
 
+const defaultIdSelector = (param) => param; // 파라미터 자체가 아이디임을 의미
+export const createPromiseThunkById = (
+    type,
+    promiseCreator,
+    /* 
+        idSelector: API를 호출할 때 사용하는 파라미터에서 id를 어떻게 선택할지 정해주는 함수 
+    */
+    idSelector = defaultIdSelector
+) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    /* 파라미터는 1개라고 가정한다. 단, 여러 개의 인잣값을 전달해야 하면 객체로 구성하여 한 개로 구성한다. */
+    const thunkCreator = (param) => async (dispatch) => {
+        const id = idSelector(param);
+        // FSA (Flux Standard Action) 규칙을 따랐다.
+        // github.com/redux-utilities/flux-standard-action
+        dispatch({ type, meta: id });
+        try {
+            const payload = await promiseCreator(param);
+            dispatch({
+                type: SUCCESS,
+                payload,
+                meta: id,
+            });
+        } catch (error) {
+            dispatch({
+                type: ERROR,
+                payload: error,
+                error: true,
+                meta: id,
+            });
+        }
+    };
+
+    return thunkCreator;
+};
+
 export const handleAsyncActions = (type, key, keepData) => {
     const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
     return (state, action) => {
@@ -53,6 +90,43 @@ export const handleAsyncActions = (type, key, keepData) => {
     };
 };
 
+export const handleAsyncActionsById = (type, key, keepData) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return (state, action) => {
+        const id = action.meta;
+        // update
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.loading(
+                            keepData ? state[key][id] && state[key][id].data : null
+                        ),
+                    },
+                };
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.success(action.payload),
+                    },
+                };
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: {
+                        ...state[key],
+                        [id]: reducerUtils.error(action.payload),
+                    },
+                };
+            default:
+                return state;
+        }
+    };
+};
 export const reducerUtils = {
     initial: (data = null) => ({
         data,
--- 01-11-redux-middleware/src/modules/posts.js
+++ 01-11-redux-middleware/src/modules/posts.js
@@ -1,5 +1,11 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, handleAsyncActions, reducerUtils } from '../lib/asyncUtils';
+import {
+    createPromiseThunk,
+    createPromiseThunkById,
+    handleAsyncActions,
+    handleAsyncActionsById,
+    reducerUtils,
+} from '../lib/asyncUtils';
 
 // 포스트 복수 개 불러오기
 // 특정 요청이 시작되었을 떄를 알리는 액션
@@ -17,20 +23,9 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 const CLEAR_POST = 'CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = (id) => async (dispatch) => {
-    dispatch({ type: GET_POST, meta: id });
-    try {
-        const payload = await postsAPI.getPostById(id);
-        dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
-    } catch (error) {
-        dispatch({
-            type: GET_POST_ERROR,
-            payload: error,
-            error: true,
-            meta: id,
-        });
-    }
-};
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
+export const clearPost = () => ({ type: CLEAR_POST });
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
@@ -40,37 +35,7 @@ const initialState = {
 };
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
-const getPostReducer = (state, action) => {
-    const id = action.meta;
-    switch (action.type) {
-        case GET_POST:
-            return {
-                ...state,
-                post: {
-                    ...state.post,
-                    [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
-                },
-            };
-        case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post: {
-                    ...state.post,
-                    [id]: reducerUtils.success(action.payload),
-                },
-            };
-        case GET_POST_ERROR:
-            return {
-                ...state,
-                post: {
-                    ...state.post,
-                    [id]: reducerUtils.error(action.payload),
-                },
-            };
-        default:
-            return state;
-    }
-};
+const getPostReducer = handleAsyncActionsById(GET_POST, 'post', true);
 
 export default function posts(state = initialState, action) {
     switch (action.type) {
