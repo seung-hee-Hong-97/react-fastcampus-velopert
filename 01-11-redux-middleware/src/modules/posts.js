8af37827709d6120a5860ddf596c5740777f6406
Sannim <inegg.apps@gmail.com>
2020-12-26 15:51:16 +0900

    Redux Thunk 함수에서 리액트 라우터 사용해 보기

--- 01-11-redux-middleware/src/modules/posts.js
+++ 01-11-redux-middleware/src/modules/posts.js
@@ -24,8 +24,9 @@ const CLEAR_POST = 'CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
-
-export const clearPost = () => ({ type: CLEAR_POST });
+export const goToHome = () => (dispatch, getState, { history }) => {
+    history.push('/');
+};
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
