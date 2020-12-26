9ad764ececc4e5b5a5577c26c8df51df1f368045
Sannim <inegg.apps@gmail.com>
2020-12-26 15:51:48 +0900

    JSON 서버 사용하기

--- 01-11-redux-middleware/src/api/posts.js
+++ 01-11-redux-middleware/src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-    {
-        id: 1,
-        title: '리덕스 미들웨어를 배워봅시다.',
-        body: '리덕스 미들웨어를 직접 만들어 보면 이해하기가 쉽죠.',
-    },
-    {
-        id: 2,
-        title: 'redux-thunk를 사용해 봅시다.',
-        body: 'redux-thunk를 사용해서 비동기 작업을 처리해 보아요',
-    },
-    {
-        id: 3,
-        title: 'redux-saga도 사용해봅니다.',
-        body: '나중에 redux-saga를 이용하여 비동기 작업을 처리하는 방법도 배워볼 거예요.',
-    },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await axios.get('http://localhost:4000/posts');
+    return response.data;
 };
 
 export const getPostById = async (id) => {
-    await sleep(500);
-    return posts.find((post) => post.id === id);
+    const response = await axios.get(`http://localhost:4000/posts/${id}`);
+    return response.data;
 };
