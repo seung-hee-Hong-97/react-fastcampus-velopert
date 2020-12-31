9142d2e49128aa89eced72a7977a54117059dafe
Sannim <inegg.apps@gmail.com>
2020-12-31 22:01:57 +0900

    타입스크립트를 이용하여 MyForm 컴포넌트 만들기

--- 01-12-typescript-with-react/src/App.tsx
+++ 01-12-typescript-with-react/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
-    return <Counter />;
+    const onSubmit = (form: { name: string; description: string }) => {
+        console.log(form);
+    };
+    return <MyForm onSubmit={onSubmit} />;
 }
 
 export default App;
