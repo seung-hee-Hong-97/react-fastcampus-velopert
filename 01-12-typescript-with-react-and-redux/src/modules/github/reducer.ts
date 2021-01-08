e2144c037dceaeb6ba72aea49c6d6e6486b9b9d5
Sannim <inegg.apps@gmail.com>
2021-01-08 22:21:57 +0900

    타입스크립트와 리덕스 미들웨어 사용하기 - reducer 최적화

--- 01-12-typescript-with-react-and-redux/src/modules/github/reducer.ts
+++ 01-12-typescript-with-react-and-redux/src/modules/github/reducer.ts
@@ -1,40 +1,20 @@
 import { createReducer } from 'typesafe-actions';
-import { GET_USER_PROFILE, GET_USER_PROFILE_ERROR, GET_USER_PROFILE_SUCCESS } from './actions';
+import { asyncState, createAsyncReducer, transformToArray } from '../../lib/reducerUtils';
+import {
+    getUserProfileAsync,
+    GET_USER_PROFILE,
+    GET_USER_PROFILE_ERROR,
+    GET_USER_PROFILE_SUCCESS,
+} from './actions';
 import { GithubAction, GithubState } from './types';
 
 const initialState: GithubState = {
-    userProfile: {
-        loading: false,
-        data: null,
-        error: null,
-    },
+    userProfile: asyncState.initial(),
 };
 
-const github = createReducer<GithubState, GithubAction>(initialState, {
-    [GET_USER_PROFILE]: (state /*, action*/) => ({
-        ...state,
-        userProfile: {
-            loading: true,
-            error: null,
-            data: null,
-        },
-    }),
-    [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
-        ...state,
-        userProfile: {
-            loading: false,
-            error: null,
-            data: action.payload,
-        },
-    }),
-    [GET_USER_PROFILE_ERROR]: (state, action) => ({
-        ...state,
-        userProfile: {
-            loading: false,
-            error: action.payload,
-            data: null,
-        },
-    }),
-});
+const github = createReducer<GithubState, GithubAction>(initialState).handleAction(
+    transformToArray(getUserProfileAsync),
+    createAsyncReducer(getUserProfileAsync, 'userProfile')
+);
 
 export default github;
