25bb73638a4e1accf1adbd7597cf23f38846148d
Sannim <inegg.apps@gmail.com>
2021-03-08 20:10:45 +0900

    useState

--- 02-05-react-beginner/src/App.js
+++ 02-05-react-beginner/src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <p>리액트 프로젝트 만들기</p>
-                <Hello name='react' color='red' isSpecial={true} />
-                <Hello color='pink' />
-                <Wrapper>뤠퍼</Wrapper>I ❤ React
-                <div className='gray-box'></div>
-            </header>
+            <Counter />
         </div>
     );
 }
