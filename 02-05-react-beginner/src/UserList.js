bdda5e439b9ec6a6eb4b8ee3bdb9c1297c006711
Sannim <inegg.apps@gmail.com>
2021-03-10 21:23:29 +0900

    useEffect 예제 및 필기

--- 02-05-react-beginner/src/UserList.js
+++ 02-05-react-beginner/src/UserList.js
@@ -1,37 +1,39 @@
 import React, { useEffect } from 'react';
 
-function User({ user, onRemove }) {
-    const { username, email, id } = user;
+function User({ user, onRemove, onToggle }) {
+    const { username, email, id, active } = user;
 
     useEffect(() => {
-        console.log(username, '컴포넌트가 화면에 나타났음');
-        /*
-            props => state
-            REST API
-            D3 Video.js
-            setInterval, setTimout
-        */
+        console.log('user값이 설정됨');
+        console.log(user);
         return () => {
-            //  clearInterval, clearTimeout
-            //  라이브러리 인스턴스 제거 작업 등을 수행
-            console.log(username, '컴포넌트가 화면에서 사라진다');
+            console.log('user값이 바뀌기 전');
+            console.log(user);
         };
-    }, []);
+    }, [user]);
 
     return (
         <div>
-            <b>{username}</b>
+            <b
+                style={{
+                    color: active ? 'green' : 'black',
+                    cursor: 'pointer',
+                }}
+                onClick={() => onToggle(id)}
+            >
+                {username}
+            </b>
             <span>({email})</span>
             <button onClick={() => onRemove(id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map((user) => (
-                <User key={user.id} user={user} onRemove={onRemove} />
+                <User key={user.id} user={user} onRemove={onRemove} onToggle={onToggle} />
             ))}
         </div>
     );
