8112b821f1be626406e53fe5f60956671d43d2ec
Sannim <inegg.apps@gmail.com>
2021-03-10 21:23:10 +0900

    useEffect 사용 예제

--- 02-05-react-beginner/src/UserList.js
+++ 02-05-react-beginner/src/UserList.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove }) {
     const { username, email, id } = user;
+
+    useEffect(() => {
+        console.log(username, '컴포넌트가 화면에 나타났음');
+        /*
+            props => state
+            REST API
+            D3 Video.js
+            setInterval, setTimout
+        */
+        return () => {
+            //  clearInterval, clearTimeout
+            //  라이브러리 인스턴스 제거 작업 등을 수행
+            console.log(username, '컴포넌트가 화면에서 사라진다');
+        };
+    }, []);
+
     return (
         <div>
             <b>{username}</b>
