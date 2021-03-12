a2cca12e77e3df8b1d731f65a711ab05df03127f
Sannim <inegg.apps@gmail.com>
2021-03-12 21:53:33 +0900

    useReducer를 이용하여 커스텀 훅 만들기

--- 02-05-react-beginner/src/useInputs.js
+++ 02-05-react-beginner/src/useInputs.js
@@ -1,20 +1,35 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useReducer } from 'react';
 
 function reducer(state, action) {
     // CHANGE
     // RESET
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value,
+            };
+        case 'RESET':
+            return action.initialForm;
+        default:
+            throw new Error('Unhandled Exception');
+    }
 }
 
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
 
     const onChange = useCallback((e) => {
         const { name, value } = e.target;
-        setForm((form) => ({ ...form, [name]: value }));
+        dispatch({
+            type: 'CHANGE',
+            name,
+            value,
+        });
     }, []);
 
     const reset = useCallback(() => {
-        setForm(initialForm);
+        dispatch({ type: 'RESET', initialForm });
     }, [initialForm]);
 
     return [form, onChange, reset];
