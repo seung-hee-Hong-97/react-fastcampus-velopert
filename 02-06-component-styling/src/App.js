db024897defb1e9939fddd50794f8586b840b49c
Sannim <inegg.apps@gmail.com>
2021-03-16 05:26:09 +0900

    Styling - 프로젝트 세팅 및  App SCSS 및 버튼 SCSS 만들기

---
+++ 02-06-component-styling/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Button from './components/Button';
+import './App.scss';
+
+function App() {
+    return (
+        <div className='App'>
+            <div className='buttons'>
+                <Button>BUTTON</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
