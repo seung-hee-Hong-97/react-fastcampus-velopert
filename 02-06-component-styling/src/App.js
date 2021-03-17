f43d8a0aa0c95cb7a9f623c2bd3b5c94c52ea99b
Sannim <inegg.apps@gmail.com>
2021-03-17 21:26:25 +0900

    Button 컴포넌트를 styled-component로 작성하기

--- 02-06-component-styling/src/App.js
+++ 02-06-component-styling/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import Button from './components/Button';
 
-const Circle = styled.div`
-    width: 5rem;
-    height: 5rem;
-    background: ${(props) => props.color || 'black'};
-    border-radius: 50%;
-    ${(props) =>
-        props.huge &&
-        css`
-            width: 10rem;
-            height: 10rem;
-        `}
+const AppBlock = styled.div`
+    width: 512px;
+    margin: 0 auto;
+    margin-top: 4rem;
+    border: 1px solid black;
+    padding: 1rem;
 `;
 
 function App() {
     return (
-        <div className='App'>
-            <Circle />
-            <Circle color='blue' huge />
-        </div>
+        <AppBlock>
+            <Button>BUTTON</Button>
+        </AppBlock>
     );
 }
 
--- 02-06-component-styling/src/components/Button.js
+++ 02-06-component-styling/src/components/Button.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import classNames from 'classnames';
-import './Button.scss';
+import styled from 'styled-components';
 
-// size: large, medium, small
-// color: blue, pink, gray
-function Button({ children, size, color, outline, fullWidth, className, ...rest }) {
-    return (
-        <button
-            className={classNames(
-                'Button',
-                size,
-                color,
-                {
-                    outline,
-                    fullWidth,
-                },
-                className
-            )}
-            {...rest}
-        >
-            {children}
-        </button>
-    );
+const StyledButton = styled.button`
+    /* 공통 스타일 */
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    /* 크기 */
+    height: 2.25rem;
+    font-size: 1rem;
+    /* 색상 */
+    background: #228be6;
+    &:hover {
+        background: #339af0;
+    }
+    &:active {
+        background: #1c7ed6;
+    }
+    /* 기타 */
+    & + & {
+        margin-left: 1rem;
+    }
+`;
+
+function Button({ children, ...rest }) {
+    return <StyledButton {...rest}>{children}</StyledButton>;
 }
-
-Button.defaultProps = {
-    size: 'medium',
-    color: 'blue',
-};
-
 export default Button;
