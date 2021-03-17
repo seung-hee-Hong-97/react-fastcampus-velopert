2ca93240e268f4af35ebc1cb86b22827a1fff537
Sannim <inegg.apps@gmail.com>
2021-03-17 21:26:01 +0900

    styled-components 사용하기

--- 02-06-component-styling/src/App.js
+++ 02-06-component-styling/src/App.js
@@ -1,61 +1,24 @@
-import logo from './logo.svg';
-import Button from './components/Button';
-import './App.scss';
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Circle = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: ${(props) => props.color || 'black'};
+    border-radius: 50%;
+    ${(props) =>
+        props.huge &&
+        css`
+            width: 10rem;
+            height: 10rem;
+        `}
+`;
 
 function App() {
     return (
         <div className='App'>
-            <div className='buttons'>
-                <Button size='large'>BUTTON</Button>
-                <Button>BUTTON</Button>
-                <Button size='small'>BUTTON</Button>
-            </div>
-            <div className='buttons'>
-                <Button size='large' color='gray'>
-                    BUTTON
-                </Button>
-                <Button color='gray'>BUTTON</Button>
-                <Button size='small' color='gray'>
-                    BUTTON
-                </Button>
-            </div>
-            <div className='buttons'>
-                <Button size='large' color='pink'>
-                    BUTTON
-                </Button>
-                <Button color='pink'>BUTTON</Button>
-                <Button size='small' color='pink'>
-                    BUTTON
-                </Button>
-            </div>
-            <div className='buttons'>
-                <Button size='large' color='blue' outline>
-                    BUTTON
-                </Button>
-                <Button color='gray' outline>
-                    BUTTON
-                </Button>
-                <Button size='small' color='pink' outline>
-                    BUTTON
-                </Button>
-            </div>
-            <div className='buttons'>
-                <Button
-                    size='large'
-                    color='blue'
-                    fullWidth
-                    onClick={() => console.log('클릭')}
-                    onMouseMove={() => console.log('마우스움직임')}
-                >
-                    BUTTON
-                </Button>
-                <Button size='large' color='gray' fullWidth>
-                    BUTTON
-                </Button>
-                <Button size='large' color='pink' fullWidth>
-                    BUTTON
-                </Button>
-            </div>
+            <Circle />
+            <Circle color='blue' huge />
         </div>
     );
 }
