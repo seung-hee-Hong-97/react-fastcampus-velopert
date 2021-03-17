7b825faa73114a1645af7a10732f84a02626db19
Sannim <inegg.apps@gmail.com>
2021-03-17 20:55:03 +0900

    outline, fullWidth 속성 만들기

--- 02-06-component-styling/src/components/Button.js
+++ 02-06-component-styling/src/components/Button.js
@@ -4,8 +4,17 @@ import './Button.scss';
 
 // size: large, medium, small
 // color: blue, pink, gray
-function Button({ children, size, color }) {
-    return <button className={classNames('Button', size, color)}>{children}</button>;
+function Button({ children, size, color, outline, fullWidth }) {
+    return (
+        <button
+            className={classNames('Button', size, color, {
+                outline,
+                fullWidth,
+            })}
+        >
+            {children}
+        </button>
+    );
 }
 
 Button.defaultProps = {
