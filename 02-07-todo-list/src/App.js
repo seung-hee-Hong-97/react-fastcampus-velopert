8622e4fe223cc302efb67eb526fb1da7fcf634fc
Sannim <inegg.apps@gmail.com>
2021-03-22 20:08:33 +0900

    Todo App에서 Context API 사용하기

--- 02-07-todo-list/src/App.js
+++ 02-07-todo-list/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -13,14 +13,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
     return (
-        <>
+        <TodoProvider>
             <GlobalStyle />
             <TodoTemplate>
                 <TodoHead />
                 <TodoList />
                 <TodoCreate />
             </TodoTemplate>
-        </>
+        </TodoProvider>
     );
 }
 
