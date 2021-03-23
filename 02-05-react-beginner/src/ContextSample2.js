ff1bc281d2f6bdee1db191021647b06e22267191
Sannim <inegg.apps@gmail.com>
2021-03-24 06:11:55 +0900

    Context API 복습1-2 (Context를 이용하여 값 넘기기)

--- 02-05-react-beginner/src/ContextSample2.js
+++ 02-05-react-beginner/src/ContextSample2.js
@@ -1,6 +1,9 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-function Child({ text }) {
+const MyContext = createContext('defaultValue');
+
+function Child() {
+    const text = useContext(MyContext); // context에 있는 값을 읽어와서 사용할 수 있게 해주는 리액트 내장 훅이다.
     return <div>안녕하세요 {text}</div>;
 }
 
@@ -13,7 +16,22 @@ function GrandParent({ text }) {
 }
 
 function ContextSample2() {
-    return <GrandParent text='GOOD' />;
+    const [value, setValue] = useState(true);
+    return (
+        // MyContext라는 컴포넌트 안에 Provider라는 컴포넌트가 또 있다.
+        <MyContext.Provider value={value ? 'GOOD' : 'BAD'}>
+            <GrandParent text='GOOD' />
+            <button onClick={() => setValue(!value)}>CLICK ME!</button>
+        </MyContext.Provider>
+    );
+
+    // /*#__PURE__*/
+    // // MyContext라는 컴포넌트 안에 Provider라는 컴포넌트가 또 있다.
+    // React.createElement(MyContext.Provider, {
+    //     value: "GOOD"
+    //   }, /*#__PURE__*/React.createElement(GrandParent, {
+    //     text: "GOOD"
+    //   }));
 }
 
 export default ContextSample2;
