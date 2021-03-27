b26267b637cb6af7c9d97e70a1913824c304e440
sannimdev <inegg.apps@gmail.com>
2021-03-27 20:12:51 +0900

    react-async 라이브러리를 이용하여 불러오기

--- 02-08-api-integrate/src/User.js
+++ 02-08-api-integrate/src/User.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+// import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+async function getUser({ id }) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
     return response.data;
 }
 
 function User({ id }) {
-    const [state] = useAsync(() => getUser(id), [id]);
-    const { loading, data: user, error } = state;
+    const { data: user, error, isLoading } = useAsync({
+        promiseFn: getUser,
+        id,
+        /* id값을 바라보고 값이 변하면 다시 호출하게 된다. */
+        watch: id,
+    });
 
-    if (loading) return <div>로딩 중...</div>;
+    if (isLoading) return <div>로딩 중...</div>;
     if (error) return <div>오류가 발생했습니다.</div>;
     if (!user) return <div>데이터 없음...</div>;
 
--- 02-08-api-integrate/src/Users.js
+++ 02-08-api-integrate/src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 
 async function getUsers() {
@@ -9,13 +9,15 @@ async function getUsers() {
 }
 
 function Users() {
-    const [state, refetch] = useAsync(getUsers, [], true);
     const [userId, setUserId] = useState(null);
-    const { loading, data: users, error } = state;
+    const { data: users, error, isLoading, reload, run } = useAsync({
+        // promiseFn: getUsers,
+        deferFn: getUsers,
+    });
 
-    if (loading) return <div>로딩 중...</div>;
+    if (isLoading) return <div>로딩 중...</div>;
     if (error) return <div>오류가 발생했습니다. {JSON.stringify(error)}</div>;
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={run}>불러오기</button>;
 
     return (
         <>
@@ -27,7 +29,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={reload}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
