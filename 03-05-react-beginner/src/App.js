25c1f5538256b52868f155ff21b6655a07347505
sannimdev <inegg.apps@gmail.com>
2021-06-14 20:34:53 +0900

    UserList에서 user 추가하기

--- 03-05-react-beginner/src/App.js
+++ 03-05-react-beginner/src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+
+    const { username, email } = inputs;
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+        },
+    ]);
+
+    // 리렌더링되지 않는 값으로 관리할 수도 있음.
+    const nextId = useRef(4);
+
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers([...users, user]);
+        setInputs({
+            username: '',
+            email: '',
+        });
+        nextId.current += 1;
+    };
+
     return (
-        <div className='App'>
-            <UserList />
-        </div>
+        <>
+            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+            <UserList users={users} />
+        </>
     );
 }
 
