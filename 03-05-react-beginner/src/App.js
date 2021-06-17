dbe8df8da2c37fa29012b058c5842088a5123536
sannimdev <inegg.apps@gmail.com>
2021-06-17 20:50:55 +0900

    useReducer를 이용하여 UserList 작성하기

--- 03-05-react-beginner/src/App.js
+++ 03-05-react-beginner/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useReducer, useRef } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,25 +7,44 @@ function countActiveUsers(users) {
     return users.filter((user) => user.active).length;
 }
 
-function App() {
-    const [inputs, setInputs] = useState({
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.name]: action.value,
+                },
+            };
+        case 'CREATE_USER':
+            return {
+                inputs: initialState.inputs,
+                users: state.users.concat(action.user),
+            };
+        case 'TOGGLE_USER':
+            return {
+                ...state,
+                users: state.users.map((user) =>
+                    user.id === action.id ? { ...user, active: !user.active } : user
+                ),
+            };
+        case 'REMOVE_USER':
+            return {
+                ...state,
+                users: state.users.filter((user) => user.id !== action.id),
+            };
+        default:
+            throw new Error('Unhandled action');
+    }
+}
+
+const initialState = {
+    inputs: {
         username: '',
         email: '',
-    });
-
-    const { username, email } = inputs;
-    const onChange = useCallback(
-        (e) => {
-            const { name, value } = e.target;
-            setInputs({
-                ...inputs,
-                [name]: value,
-            });
-        },
-        [inputs]
-    );
-
-    const [users, setUsers] = useState([
+    },
+    users: [
         {
             id: 1,
             username: 'velopert',
@@ -44,51 +63,54 @@ function App() {
             email: 'liz@example.com',
             active: false,
         },
-    ]);
+    ],
+};
 
-    // 리렌더링되지 않는 값으로 관리할 수도 있음.
+function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
     const nextId = useRef(4);
+    const { users } = state;
+    const { username, email } = state.inputs;
 
-    // 컴포넌트가 리렌더링될 때마다 새로운 함수를 만들고 있었다.
+    const onChange = useCallback((e) => {
+        const { name, value } = e.target;
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name,
+            value,
+        });
+    }, []);
+
     const onCreate = useCallback(() => {
-        const user = {
-            id: nextId.current,
-            username,
-            email,
-        };
-        setUsers((users) => [...users, user]);
-        setInputs({
-            username: '',
-            email: '',
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email,
+            },
         });
         nextId.current += 1;
     }, [username, email]);
 
-    const onRemove = useCallback((id) => {
-        setUsers((users) => users.filter((user) => user.id !== id));
-    }, []);
-
     const onToggle = useCallback((id) => {
-        setUsers((users) =>
-            users.map((user) =>
-                user.id === id
-                    ? {
-                          ...user,
-                          active: !user.active,
-                      }
-                    : user
-            )
-        );
+        dispatch({
+            type: 'TOGGLE_USER',
+            id,
+        });
+    }, []);
+    const onRemove = useCallback((id) => {
+        dispatch({
+            type: 'REMOVE_USER',
+            id,
+        });
     }, []);
 
-    // 특정 값이 바뀔 때만 특정 함수를 실행하여 연산 / 원하는 값이 바뀌지 않았다면 값을 재사용
-    // ex) input상자에 텍스트를 입력할 때마다 리렌더링이되는데 이때는 활성 사용자 수의 상태가 변하지 않으므로 함수를 재실행하지않고 값을 재사용한다.
-    const count = useMemo(() => countActiveUsers(users), [users]);
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-            <div>활성 사용자 수: {count}</div>
+            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+            <div>활성 사용자 수: {users.filter((user) => user.active).length}</div>
         </>
     );
 }
--- 03-05-react-beginner/src/index.js
+++ 03-05-react-beginner/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Counter from './Counter';
+import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <Counter />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
