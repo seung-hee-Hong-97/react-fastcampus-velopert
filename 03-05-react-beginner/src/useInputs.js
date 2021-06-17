4c4861b53fbf7b709fae8f594bd95b43124391d2
sannimdev <inegg.apps@gmail.com>
2021-06-17 21:48:24 +0900

    커스텀 훅 내 상태 변화를 useState에서 useReducer로 변환하기

--- 03-05-react-beginner/src/useInputs.js
+++ 03-05-react-beginner/src/useInputs.js
@@ -1,14 +1,35 @@
-import { useState, useCallback } from 'react';
+import { useCallback, useReducer } from 'react';
 
 // 커스텀 훅은 자주 사용되는 컴포넌트의 반복되는 로직을 포함시킨 내용이라고 생각하자.
 // 커스텀 훅을 만들 때는  use라는 키워드로 시작해서 기능을 나타내는 단어를 선정하여 함수를 만듦.
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value,
+            };
+        case 'RESET':
+            return {
+                ...action.initialForm,
+            };
+        default:
+            throw new Error('Unhanlded Error');
+    }
+}
+
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
     const onChange = useCallback((e) => {
         const { name, value } = e.target;
-        setForm((form) => ({ ...form, [name]: value }));
+        dispatch({
+            type: 'CHANGE',
+            name,
+            value,
+        });
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    const reset = useCallback(() => dispatch({ type: 'RESET', initialForm }), [initialForm]);
 
     return [form, onChange, reset];
 }
