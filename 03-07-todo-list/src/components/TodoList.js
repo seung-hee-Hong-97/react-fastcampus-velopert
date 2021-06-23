acee203130790f95abafd06536983ef78e6cc5b5
sannimdev <inegg.apps@gmail.com>
2021-06-23 20:49:39 +0900

    Context를 이용하여 상태관리를 수행하는 Todo App 완성하기
    - useContext 커스텀 훅을 이용하여 TodoApp을 만든다.

--- 03-07-todo-list/src/components/TodoList.js
+++ 03-07-todo-list/src/components/TodoList.js
@@ -11,14 +11,13 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    const state = useTodoState();
+    const todos = useTodoState();
 
     return (
         <TodoListBlock>
-            <TodoItem text='프로젝트 작성하기' done={true} />
-            <TodoItem text='컴포넌트 스타일링하기' done={true} />
-            <TodoItem text='Context 만들기' done={false} />
-            <TodoItem text='기능 구현하기' done={false} />
+            {todos.map((todo) => (
+                <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done} />
+            ))}
         </TodoListBlock>
     );
 }
