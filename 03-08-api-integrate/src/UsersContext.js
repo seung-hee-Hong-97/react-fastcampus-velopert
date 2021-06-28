c60af0a1cedb74e1be4f2ed337a28949f6acc459
sannimdev <inegg.apps@gmail.com>
2021-06-29 05:30:28 +0900

    handler를 만들어서 리팩토링하기

--- 03-08-api-integrate/src/UsersContext.js
+++ 03-08-api-integrate/src/UsersContext.js
@@ -1,37 +1,11 @@
 import React, { createContext, useReducer, useContext } from 'react';
 import * as api from './api';
-import createAsyncDispatcher from './asyncActionUtils';
+import createAsyncDispatcher, { createAsyncHandler, initialAsyncState } from './asyncActionUtils';
 
 const initialState = {
-    users: {
-        loading: false,
-        data: null,
-        error: null,
-    },
-    user: {
-        loading: false,
-        data: null,
-        error: null,
-    },
+    users: initialAsyncState,
+    user: initialAsyncState,
 };
-
-const loadingState = {
-    loading: true,
-    data: null,
-    error: null,
-};
-
-const success = (data) => ({
-    loading: false,
-    data,
-    error: null,
-});
-
-const error = (error) => ({
-    loading: false,
-    data: null,
-    error,
-});
 
 // GET_USERS
 // GET_USERS_SUCCESS
@@ -41,38 +15,19 @@ const error = (error) => ({
 // GET_USER_SUCCESS
 // GET_USER_ERROR
 
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
+
 function usersReducer(state, action) {
     switch (action.type) {
         case 'GET_USERS':
-            return {
-                ...state,
-                users: loadingState,
-            };
         case 'GET_USERS_SUCCESS':
-            return {
-                ...state,
-                users: success(action.data),
-            };
         case 'GET_USERS_ERROR':
-            return {
-                ...state,
-                users: error(action.error),
-            };
+            return usersHandler(state, action);
         case 'GET_USER':
-            return {
-                ...state,
-                user: loadingState,
-            };
         case 'GET_USER_SUCCESS':
-            return {
-                ...state,
-                user: success(action.data),
-            };
         case 'GET_USER_ERROR':
-            return {
-                ...state,
-                user: error(action.error),
-            };
+            return userHandler(state, action);
         default:
             throw new Error('Unhandled action type', action.type);
     }
--- 03-08-api-integrate/src/asyncActionUtils.js
+++ 03-08-api-integrate/src/asyncActionUtils.js
@@ -20,3 +20,56 @@ export default function createAsyncDispatcher(type, promiseFn) {
 
     return actionHandler;
 }
+
+export const initialAsyncState = {
+    loading: false,
+    data: null,
+    error: null,
+};
+
+const loadingState = {
+    loading: true,
+    data: null,
+    error: null,
+};
+
+const success = (data) => ({
+    loading: false,
+    data,
+    error: null,
+});
+
+const error = (error) => ({
+    loading: false,
+    data: null,
+    error,
+});
+
+export function createAsyncHandler(type, key) {
+    const SUCCESS = `${type}_SUCCESS`;
+    const ERROR = `${type}_ERROR`;
+
+    function handler(state, action) {
+        switch (action.type) {
+            case type:
+                return {
+                    ...state,
+                    [key]: loadingState,
+                };
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]: success(action.data),
+                };
+            case ERROR:
+                return {
+                    ...state,
+                    [key]: error(action.error),
+                };
+            default:
+                return state;
+        }
+    }
+
+    return handler;
+}
