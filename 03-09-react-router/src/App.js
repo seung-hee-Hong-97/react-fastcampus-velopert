6943f87c51f7171699b90ae8befacfed512f78af
sannimdev <inegg.apps@gmail.com>
2021-07-02 08:57:58 +0900

    Sub routing

--- 03-09-react-router/src/App.js
+++ 03-09-react-router/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
     return (
@@ -15,12 +15,14 @@ function App() {
                     <Link to='/about?a=1'>소개</Link>
                 </li>
                 <li>
-                    <Link to='/profile/homer'>심슨 아저씨</Link>
+                    {/* <Link to='/profile/homer'>심슨 아저씨</Link> */}
+                    <Link to='/profiles'>프로필 목록</Link>
                 </li>
             </ul>
             <Route path='/' component={Home} exact />
             <Route path='/about' component={About} />
-            <Route path='/profile/:username' component={Profile} />
+            {/* <Route path='/profile/:username' component={Profile} /> */}
+            <Route path='/profiles' component={Profiles} />
         </div>
     );
 }
---
+++ 03-09-react-router/src/Profiles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Profile from './Profile';
+import { Link, Route } from 'react-router-dom';
+
+function Profiles() {
+    return (
+        <div>
+            <h3>사용자 목록</h3>
+            <ul>
+                <li>
+                    <Link to='/profiles/hong'>hong</Link>
+                </li>
+                <li>
+                    <Link to='/profiles/homer'>Homer</Link>
+                </li>
+            </ul>
+            <hr />
+            <Route
+                Path='/profiles'
+                exact
+                render={() => (
+                    <div>
+                        사용자 선택해주세용
+                        <hr />
+                    </div>
+                )}
+            />
+            {/* path를 Path로 쓰면 어떻게 해... ㅠㅠㅠㅠ */}
+            <Route path='/profiles/:username' component={Profile} />
+        </div>
+    );
+}
+
+export default Profiles;
