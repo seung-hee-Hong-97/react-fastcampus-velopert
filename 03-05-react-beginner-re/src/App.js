c73ee47a7333e343d480fbf84e3b7f24103d7804
sannimdev <inegg.apps@gmail.com>
2021-07-08 07:52:30 +0900

    불변성 복습하기

--- 03-05-react-beginner-re/src/App.js
+++ 03-05-react-beginner-re/src/App.js
@@ -1,9 +1,9 @@
-import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
     return (
         <div className='App'>
-            <Counter />
+            <InputSample />
         </div>
     );
 }
